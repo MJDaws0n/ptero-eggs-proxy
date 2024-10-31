eadbec5ad2cc116bc2b2e5aab6b3557ff91e67fa
MJDawson <66313685+MJDaws0n@users.noreply.github.com>
2024-10-31 17:04:04 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,16 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-echo "⟳ Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+if [ -z "$1" ]; then
+    echo "Error: No domain provided! Please provide one."
+else
+    # Update the 15th line of ./proxymanage.php with the provided domain
+    sed -i "15s/.*/\$DOMAIN = '$1';/" ./proxymanage.php
 
-echo "⟳ Starting Nginx..."
-echo "✓ Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+    echo "⟳ Starting PHP-FPM..."
+    /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+    echo "⟳ Starting Nginx..."
+    echo "✓ Successfully started"
+    /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+fi
